58b92db2a57f683cff44e58c83f8a63f5db5300d
Samarth Gupta <livesamarthgupta@gmail.com>
2024-09-24 23:39:48 +0530

    dev2 done

--- src/main/java/com/club/eliteclub/EliteClubApplication.java
+++ src/main/java/com/club/eliteclub/EliteClubApplication.java
@@ -25,6 +25,9 @@ public class EliteClubApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EliteClubApplication.class, args);
+		// these lines are added becuase of dev2
+		// dev2
+		// dev2
 	}
 
 	@Override
